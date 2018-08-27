ab14c540945cadb72aeaa0af78a97ad5bd900018
ethancyx <617638376@qq.com>
2018-08-27 20:40:08 +0800

    完成遍历省市县三级列表功能

--- app/src/main/java/com/android/ethan/coolweather/activity/MainActivity.java
+++ app/src/main/java/com/android/ethan/coolweather/activity/MainActivity.java
@@ -1,8 +1,15 @@
-package com.android.ethan.coolweather;
+package com.android.ethan.coolweather.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.ethan.coolweather.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +18,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
