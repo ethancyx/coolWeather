6bef8aec4e1e346f0208027c250006611b5d3e42
ethancyx <617638376@qq.com>
2018-08-29 00:01:29 +0800

    bug修改

--- app/src/main/java/com/android/ethan/coolweather/activity/WeatherActivity.java
+++ app/src/main/java/com/android/ethan/coolweather/activity/WeatherActivity.java
@@ -11,6 +11,7 @@ import okhttp3.Response;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.graphics.ImageFormat;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -53,13 +54,14 @@ public class WeatherActivity extends AppCompatActivity {
 
     public DrawerLayout drawerLayout;
     private Button navButton;
+    private String nowCityId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (Build.VERSION.SDK_INT>=21){
             View decorView =getWindow().getDecorView();
-            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN |View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
             getWindow().setStatusBarColor(Color.TRANSPARENT);
         }
         setContentView(R.layout.activity_weather);
@@ -99,7 +101,6 @@ public class WeatherActivity extends AppCompatActivity {
             showWeatherInfo(weather);
         }else {
             mWeatherId=getIntent().getStringExtra("weather_id");
-            String  weatherId =getIntent().getStringExtra("weather_id");
             weatherLayout.setVisibility(View.INVISIBLE);
             requestWeather(mWeatherId);
         }
@@ -107,7 +108,13 @@ public class WeatherActivity extends AppCompatActivity {
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
+                if (nowCityId==mWeatherId){
                 requestWeather(mWeatherId);
+                }else {
+                    requestWeather(nowCityId);
+                }
+                Log.e("TAG",nowCityId);
+                Log.e("TAG",mWeatherId);
             }
         });
 
@@ -145,13 +152,13 @@ public class WeatherActivity extends AppCompatActivity {
     }
 
     private void showWeatherInfo(Weather weather) {
-
+        nowCityId =weather.basic.weatherId;
         String cityName =weather.basic.cityname;
         String updateTime =weather.basic.update.updateTime.split(" ")[1];
         String degree =weather.now.temperature+"℃";
         String weatherInfo =weather.now.more.info;
         titleCity.setText(cityName);
-        titleUpdateTime.setText(updateTime);
+        titleUpdateTime.setText("更新于"+updateTime);
         degreeText.setText(degree);
         weatherInfoText.setText(weatherInfo);
         forecastLayout.removeAllViews();
